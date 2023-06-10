031f5a92f5055221124994d1663575e73c5e4003
Simone Ferronato <129115031+simoneferronato@users.noreply.github.com>
2023-06-10 21:58:20 +0100

    create ex010

--- exercicios/aula11/ex008.js
+++ exercicios/aula11/ex008.js
@@ -1,2 +1,6 @@
-var vel = 60.50
+var vel = 78.50
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel>60) { //condição simples, não existe um else
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
