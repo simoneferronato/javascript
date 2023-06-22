0d22243bac9510a32923beca504c43e57865f4be
Simone Ferronato <129115031+simoneferronato@users.noreply.github.com>
2023-06-22 11:35:49 +0100

    Create ambiente.js

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}
+    
+
+/* while (c <= 500) {
+    console.log('Tudo bem?')
+    c++
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+
+
