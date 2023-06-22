bb0208108d0d4d4300e0707fa9791ba2b9bd1744
Simone Ferronato <129115031+simoneferronato@users.noreply.github.com>
2023-06-22 12:01:45 +0100

    update ambiente.js

--- exercicios/aula13/ambiente.js
+++ exercicios/aula13/ambiente.js
@@ -1,16 +1,27 @@
 var c = 1
 
-while (c <= 5) {
+// Estrutura de Repetição com teste lógico no final
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+/* Estrutura de Repetição com teste lógico no início
+while (c <= 6) {
     console.log(`Passo ${c}`)
     c++
 }
-    
+*/   
 
-/* while (c <= 500) {
+
+/* Estrutura de Repetição com teste lógico no início
+while (c <= 6) {
     console.log('Tudo bem?')
     c++
 }
 */
+
 
 /*
 console.log('Tudo bem?')
